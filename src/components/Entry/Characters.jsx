38011aaa66fcab64649657b0d43cbe65a62828b1
David Abejón <davidabejonheras@gmail.com>
2025-08-24 17:23:30 +0200

    fix. characters tab load

--- src/components/Entry/Characters.jsx
+++ src/components/Entry/Characters.jsx
@@ -3,6 +3,8 @@ import { capitalize } from "../../helper"
 import Card from "../Card"
 import { getCharactersPage } from "../../api/queries"
 import { useParams } from "react-router-dom"
+import '../../styles/Characters.css'
+import { Button } from "antd"
 
 function Characters() {
 
@@ -10,71 +12,71 @@ function Characters() {
   const { type } = useParams()
 
   const [characters, setCharacters] = useState([])
+  const [hasNextPage, setHasNextPage] = useState(true)
+  const [page, setPage] = useState(1)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const fetchAllCharacters = async () => {
+    const fetchCharacters = async () => {
       let allChars = []
-      let page = 1
-      let hasNextPage = true
 
-      while (hasNextPage) {
-        const response = await fetch("https://graphql.anilist.co", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-          },
-          body: JSON.stringify({
-            query: getCharactersPage,
-            variables: { id: id, type: type.toUpperCase(), page, perPage: 25 },
-          }),
-        })
+      const response = await fetch("https://graphql.anilist.co", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          query: getCharactersPage,
+          variables: { id: id, type: type.toUpperCase(), page, perPage: 25 },
+        }),
+      })
 
-        const data = await response.json()
-        const charData = data.data.Media.characters
+      const data = await response.json()
+      const charData = data.data.Media.characters
 
-        allChars = [
-          ...allChars,
-          ...charData.edges.map(edge => ({
-            ...edge.node,
-            role: edge.role,
-            voiceActors: edge.voiceActors,
-          })),
-        ]
+      allChars = [
+        ...characters,
+        ...charData.edges.map(edge => ({
+          ...edge.node,
+          role: edge.role,
+          voiceActors: edge.voiceActors,
+        })),
+      ]
 
-        hasNextPage = charData.pageInfo.hasNextPage
-        page++
-      }
+      setHasNextPage(charData.pageInfo.hasNextPage)
 
       setLoading(false)
       setCharacters(allChars)
     }
 
     setLoading(true)
-    fetchAllCharacters()
-  }, [id, type])
+    fetchCharacters()
+  }, [id, type, page])
 
   return (
     <>
-      {loading ? <p>Loading...</p> : (
-        <div className="flex flex-wrap gap-4">
-          {characters.map(character => (
-            <Card
-              key={character.id}
-              imageSrc={character.image.medium}
-              secondImageSrc={character.voiceActors[0]?.image?.medium}
-              title={character.name.full || character.name.native}
-              secondTitle={character.voiceActors[0]?.name?.full || character.voiceActors[0]?.name?.native}
-              subtitle={capitalize(character.role)}
-              secondSubtitle={capitalize(character.voiceActors[0]?.languageV2)}
-              type={"double"}
-            />
-          ))}
-        </div>
+      <div className="flex flex-wrap gap-4">
+        {characters.map(character => (
+          <Card
+            key={character.id}
+            imageSrc={character.image.medium}
+            secondImageSrc={character.voiceActors[0]?.image?.medium}
+            title={character.name.full || character.name.native}
+            secondTitle={character.voiceActors[0]?.name?.full || character.voiceActors[0]?.name?.native}
+            subtitle={capitalize(character.role)}
+            secondSubtitle={capitalize(character.voiceActors[0]?.languageV2)}
+            type={"double"}
+          />
+        ))}
+      </div>
+      {hasNextPage && (
+        <Button type="primary" className="mt-5" loading={loading} onClick={() => setPage(prevPage => prevPage + 1)} disabled={!hasNextPage}>
+          Load More
+        </Button>
       )}
     </>
   )
 }
 
-      export default Characters
+export default Characters
