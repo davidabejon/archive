9100781f7fee0122ef7bd2e9ebdef19134b4de79
David Abejón <davidabejonheras@gmail.com>
2025-08-26 20:59:29 +0200

    add. toggle spoilers on and off

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Entry from './views/Entry'
 import Character from './views/Character'
+import Staff from './views/Staff'
 
 function App() {
+
+  window.addEventListener('click', event => {
+    const spoilers = document.querySelectorAll('.markdown_spoiler');
+    // if click on active spoiler, hide it
+    spoilers.forEach(spoiler => {
+      // if click was made on this spoiler
+      if (spoiler.contains(event.target)) {
+        console.log(spoiler.classList);
+        if (spoiler.classList.contains('spoiler-active')) {
+          spoiler.classList.remove('spoiler-active');
+        } else {
+          spoiler.classList.add('spoiler-active');
+        }
+      }
+    });
+  });
 
   return (
     <Router>
